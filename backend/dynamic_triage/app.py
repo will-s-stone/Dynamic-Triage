88acc6b113d86baa1ee42e5874e3eedf07a24273
will-s-stone <will@williamstone.me>
2024-09-14 16:42:44 -0400

    Set up for mongo db atlas (yes I exposed my password :0)

--- backend/dynamic_triage/app.py
+++ backend/dynamic_triage/app.py
@@ -1,20 +1,7 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from flask import Flask
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://stonew9799:0JkLuIc2ThPExqyk@hophack-cluster.ulbgg.mongodb.net/?retryWrites=true&w=majority&appName=hophack-cluster"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-db = client.your_database
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
---
+++ backend/dynamic_triage/db-handler.py
@@ -0,0 +1,18 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo import MongoClient
+import json
+
+def post(usr_infor) -> bool:
+
+uri = "mongodb+srv://stonew9799:0JkLuIc2ThPExqyk@hophack-cluster.ulbgg.mongodb.net/?retryWrites=true&w=majority&appName=hophack-cluster"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.your_database
